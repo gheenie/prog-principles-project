45688e77fdb979732aabb9b534aae41898590164
infoomics <sbx_94@hotmail.com>
2024-03-19 23:35:16 +0000

    Add test_can_move_to_king_can_move_by_capturing

--- test_chess_puzzle.py
+++ test_chess_puzzle.py
@@ -440,6 +440,24 @@ def test_can_move_to_king_can_move_to_empty_space(board1):
     assert bk23.can_move_to(1, 4, board1) is True
 
 
+def test_can_move_to_king_can_move_by_capturing(board1, board2):
+    wk35 = piece_at(3, 5, board1)
+    bk23 = piece_at(2, 3, board1)
+
+    # Capturing knight.
+    assert wk35.can_move_to(2, 4, board1) is True
+    assert bk23.can_move_to(1, 2, board1) is True
+
+    wk35.pos_x = 4
+    wk35.pos_y = 1
+    bk23.pos_x = 5
+    bk23.pos_y = 1
+    
+    # Capturing king.
+    assert wk35.can_move_to(5, 1, board2) is True
+    assert bk23.can_move_to(4, 1, board2) is True
+
+
 @pytest.mark.skip
 def test_move_to1():
     Actual_B = wn1.move_to(2,4, B1)
