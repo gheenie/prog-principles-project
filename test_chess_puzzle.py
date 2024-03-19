089709db608c74305163a18533972e7946d3490a
infoomics <sbx_94@hotmail.com>
2024-03-19 12:23:07 +0000

    Restyle board2 to fit conventions

--- test_chess_puzzle.py
+++ test_chess_puzzle.py
@@ -190,18 +190,18 @@ def board2():
     ♘ ♞ ♘
     ♞    
     '''
-    wn12 = Knight(1,2,True)
-    wn52 = Knight(5,2,True)
-    wn54 = Knight(5,4, True)
+    wn12 = Knight(1, 2, True)
+    wn52 = Knight(5, 2, True)
+    wn54 = Knight(5, 4, True)
     wn44 = Knight(4, 4, True)
     wn25 = Knight(2, 5, True)
-    wk35 = King(3,5, True)
+    wk35 = King(3, 5, True)
 
-    bn11 = Knight(1,1,False)
-    bn24 = Knight(2,4, False)
+    bn11 = Knight(1, 1,False)
+    bn24 = Knight(2, 4, False)
     bn32 = Knight(3, 2, False)
     bn43 = Knight(4, 3, False)
-    bk23 = King(2,3, False)
+    bk23 = King(2, 3, False)
 
     return (5, [wn12, bn11, wn52, bn24, wn54, wk35, bk23, wn44, wn25, bn32, bn43])
 
