36cbb86894dfa6163e4b5f92ca12317a06a789d4
infoomics <sbx_94@hotmail.com>
2024-03-19 16:44:52 +0000

    Move board3 up part 2

--- test_chess_puzzle.py
+++ test_chess_puzzle.py
@@ -320,6 +320,14 @@ def board3():
     wn25 = Knight(2, 5, True)
     wk35 = King(3, 4, True)
 
+    bn11 = Knight(1, 1,False)
+    bn24 = Knight(2, 4, False)
+    bn32 = Knight(3, 2, False)
+    bn43 = Knight(4, 3, False)
+    bk23 = King(2, 3, False)
+
+    return (5, [wn12, bn11, wn52, bn24, wn54, wk35, bk23, wn25, bn32, bn43])
+
 
 def test_can_move_to_knight_can_reach_but_checked():
     assert False
@@ -380,17 +388,6 @@ def test_is_check1():
     '''
     
     assert is_check(True, B2) == True
-
-
-
-
-    bn11 = Knight(1, 1,False)
-    bn24 = Knight(2, 4, False)
-    bn32 = Knight(3, 2, False)
-    bn43 = Knight(4, 3, False)
-    bk23 = King(2, 3, False)
-
-    return (5, [wn12, bn11, wn52, bn24, wn54, wk35, bk23, wn25, bn32, bn43])
 
 
 def test_is_check_white_king_checked_by_black_king(board3):
