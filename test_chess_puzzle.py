a14c6adafadf6c8cfab70e2f8e19fc0ea3038533
infoomics <sbx_94@hotmail.com>
2024-03-19 20:57:12 +0000

    Reposition can_move_to knight can move by capturing

--- test_chess_puzzle.py
+++ test_chess_puzzle.py
@@ -351,6 +351,23 @@ def test_can_move_to_knight_can_move_to_empty_space(board1):
     assert bn11.can_move_to(3, 2, board1) is True
 
 
+def test_can_move_to_knight_can_move_by_capturing(board1, board2):
+    wn12 = piece_at(1, 2, board1)
+    bn24 = piece_at(2, 4, board1)
+    wn44 = piece_at(4, 4, board2)
+    bn43 = piece_at(4, 3, board2)
+    board2[1].remove(bn43)
+
+    # Capturing knight.
+    assert wn12.can_move_to(2, 4, board1) is True
+    assert bn24.can_move_to(1, 2, board1) is True
+    # Capturing king.
+    assert wn44.can_move_to(2, 3, board2) is True
+    board2[1].remove(wn44)
+    board2[1].append(bn43)
+    assert bn43.can_move_to(3, 5, board2) is True
+
+
 def test_can_move_to_knight_cannot_reach_but_not_checked(board1):
     wn12 = piece_at(1, 2, board1)
     wn52 = piece_at(5, 2, board1)
@@ -385,20 +402,6 @@ def test_can_move_to_knight_can_move_by_removing_check(board2):
 
     assert wn31.can_move_to(4, 3, board2) is True
     assert bn32.can_move_to(4, 4, board2) is True
-
-
-def test_can_move_to_knight_can_move_with_capturing(board1, board2):
-    wn12 = piece_at(1, 2, board1)
-    bn24 = piece_at(2, 4, board1)
-    wn44 = piece_at(4, 4, board2)
-    bn43 = piece_at(4, 3, board2)
-
-    # Capturing knight.
-    assert wn12.can_move_to(2, 4, board1) is True
-    assert bn24.can_move_to(1, 2, board1) is True
-    # Capturing king.
-    assert wn44.can_move_to(2, 3, board2) is True
-    assert bn43.can_move_to(3, 5, board2) is True
 
 
 @pytest.mark.skip
