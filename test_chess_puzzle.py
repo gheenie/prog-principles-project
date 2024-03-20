5f93210998453dba65a5fdaf82b684e930a39ff8
infoomics <sbx_94@hotmail.com>
2024-03-20 14:30:29 +0000

    Act and assert test_move_to_white_knight_captured_a_king

--- test_chess_puzzle.py
+++ test_chess_puzzle.py
@@ -575,9 +575,21 @@ def test_move_to_white_knight_captured_a_king(board2):
     bn24 = piece_at(2, 4, board2)
     bn32 = piece_at(3, 2, board2)
     bn43 = piece_at(4, 3, board2)
-    bk23 = piece_at(2, 3, board2)
     expected_board = (5, [wn12, wn52, wn54, wn44, wn25, wk35, bn11, bn24, bn32, bn43])
     wn23 = Knight(2, 3, True)
+
+    result_board = wn44.move_to(2, 3, board2)
+    result_wn44 = piece_at(2, 3, result_board)
+
+    assert result_board[0] == expected_board[0]
+    for i in range(len(expected_board)):
+        assert result_board[1][i] is expected_board[1][i]
+    assert (
+        result_wn44.pos_x == wn23.pos_x
+        and result_wn44.pos_y == wn23.pos_y
+        and result_wn44.side == wn23.side
+        and isinstance(result_wn44, Knight) is isinstance(wn23, Knight)
+    )
 
 
 def test_move_to_black_knight_captured_a_knight():
