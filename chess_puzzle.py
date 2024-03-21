667ee839f84ede21b23a69ea7c6ec3de565e6570
infoomics <sbx_94@hotmail.com>
2024-03-21 20:38:46 +0000

    Replace can_move_to with can_reach as is_check is not required

--- chess_puzzle.py
+++ chess_puzzle.py
@@ -283,10 +283,11 @@ def is_checkmate(side: bool, B: Board) -> bool:
     # Scenario where checking piece can be eaten.
     checking_piece = None
     for piece in B[1]:
-        if piece.can_move_to(defending_king.pos_x, defending_king.pos_y, B) is True:
+        # There can only be one checking piece.
+        if piece.can_reach(defending_king.pos_x, defending_king.pos_y, B) is True:
             checking_piece = piece
     for piece in B[1]:
-        if piece.can_move_to(checking_piece.pos_x, checking_piece.pos_y, B) is True:
+        if piece.can_reach(checking_piece.pos_x, checking_piece.pos_y, B) is True:
             return False
         
     return True
