ea354f2fcb07ff5a119f7f86b5b53b8b60b0b7a2
infoomics <sbx_94@hotmail.com>
2024-03-21 18:59:02 +0000

    Add test_is_stalemate_has_empty_escape_zones for black

--- test_chess_puzzle.py
+++ test_chess_puzzle.py
@@ -1101,6 +1101,21 @@ def test_is_stalemate_getting_checked():
     assert is_stalemate(False, board) is False
 
 
+def test_is_stalemate_has_empty_escape_zones():
+    '''
+      ♘ 
+        
+        
+    ♚ ♔ 
+    '''
+    wk31 = King(3, 1, True)
+    wn34 = Knight(3, 4, True)
+    bk11 = King(1, 1, False)
+    board = (4, wk31, wn34, bk11)
+
+    assert is_stalemate(False, board) is False
+
+
 @pytest.mark.skip
 def test_read_board1():
     B = read_board("board_examp.txt")
